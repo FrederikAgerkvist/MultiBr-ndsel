11a711b0e11e2484993ca07f6d77627950eaf637
Anders Nielsen <asn@inuatek.com>
2023-12-04 16:00:27 +0100

    Newest

--- MultibrændselV2.py
+++ MultibrændselV2.py
@@ -16,8 +16,11 @@ import csv
 # oilPriceLtr = oilAmount/oilPrice # DKK pr. L
 
 #Anders variables moved form MixedBrændselwTrafi sheet Variables to here:
-start_date = '2023-10-01T00:00'
+start_date = '2023-04-01T00:00'
 end_date = '2023-11-01T00:00'
+
+
+
 minimumRunTime = 2 # 1-24 hr - NULL INDEX!
 minimumPriceDifference = 0.1 # DKK 10 øre
 efficiencyElectricity = 1 # 0-1
@@ -46,7 +49,7 @@ naturGasAfgift = 2.431 #DKK pr. m3
 kapacitetsTillæg = 0.069 #DKK pr. m3
 nødForsyningsTarif = 0.016 #DKK pr. m3
 
-gasafgift = co2afgift + noxafgift + disturbutionGas + naturGasAfgift + kapacitetsTillæg + nødForsyningsTarif
+gasafgift = (co2afgift + noxafgift + disturbutionGas + naturGasAfgift + kapacitetsTillæg + nødForsyningsTarif)/10.55
 
 lavLast = 0.0652
 højLast = 0.1957
@@ -79,6 +82,7 @@ dayCounter = 0
 #print("El afgift lav last: " + str(ElSpotPrices))
 for gas_entry in GasSpotPrices:
     gas_entry_date = datetime.strptime(gas_entry['GasDay'], "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d")
+    print(gas_entry)
 
     filtered_electricity_entries = [electricity_entry for electricity_entry in ElSpotPrices if electricity_entry['HourDK'].startswith(gas_entry_date)]
     #print(filtered_electricity_entries)
@@ -90,53 +94,65 @@ for gas_entry in GasSpotPrices:
     startEndTimeList = []
     mixedUsagePriceDay = []
     
-    fixedGasPrice = 0.5086
+    
+    #fixedGasPrice = gas_entry['EEXSpotIndexEUR_MWh']/1000*7.5 + gasafgift
+    fixedGasPrice = (gas_entry['PurchasePriceDKK_kWh'] + gasafgift) * 1.05
+    #fixedGasPrice = (gasPrisGennemsnit + gasafgift)/10.55
+    #fixedGasPrice = 0.5086
 
-    print("shift")
+
+    #print("shift")
     
     for electricity_entry in filtered_electricity_entries:
         tarif = 0
         electricity_entry_time = datetime.strptime(electricity_entry['HourDK'][-8:], "%H:%M:%S").strftime("%H:%M")
 
-        #print(electricity_entry['HourDK'][-8:], "%H:%M:%S")
-        #print(datetime.strptime(electricity_entry['HourDK'][-8:], "%H:%M:%S").strftime("%H:%M"))         
-            
-            
+        
+        #if datetime.strptime(electricity_entry['HourDK'][-8:], "%H:%M:%S").strftime("%A") == "Saturday" or datetime.strptime(electricity_entry['HourDK'][-8:], "%H:%M:%S").strftime("%A") == "Sunday":
+        #    WEEKEND = True
+        #else:
+        #    WEEKEND = False
+        
         if electricity_entry_time == '23:00':
             electricity_entry_time = '23:59'
-            
-        if '00:00' <= electricity_entry_time <= '05:00':
-            tarif = lavLast
-        elif '06:00' <= electricity_entry_time <= '16:00':
-            tarif = højLast
-        elif '21:00' <= electricity_entry_time <= '23:59':
-            tarif = højLast
+        
+        isWeekend = datetime.strptime(gas_entry_date, "%Y-%m-%d").weekday() in [0, 6]
+        is_summer = 4 <= datetime.strptime(gas_entry_date, "%Y-%m-%d").month < 10
+        
+        if (is_summer):
+            #print(gas_entry_date)
+            if '00:00' <= electricity_entry_time <= '05:00':
+                tarif = elAfgiftLavLast
+            elif '06:00' <= electricity_entry_time <= '21:00' and not isWeekend:
+                tarif = elAfgiftHøjLast
+            else:
+                tarif = elAfgiftHøjLast            
         else:
-            tarif = spidsLast
+            if '00:00' <= electricity_entry_time <= '05:00':
+                tarif = elAfgiftLavLast
+            elif '06:00' <= electricity_entry_time <= '21:00' and not isWeekend:
+                tarif = elAfgiftSpidsLast
+            else:
+                tarif = elAfgiftHøjLast
 
         ElectricitySpotPriceList.append((electricity_entry['SpotPriceDKK'] / 1000) + tarif) 
         calculatePricesWhenHour = ['02:00', '04:00', '06:00', '08:00', '10:00', '12:00', '14:00', '16:00', '18:00', '20:00', '22:00', '23:59']
         if electricity_entry_time in calculatePricesWhenHour:
             ElectricitySpotPriceAverage = round(sum(ElectricitySpotPriceList) / len(ElectricitySpotPriceList), 4)
             if ElectricitySpotPriceAverage < fixedGasPrice:
-                valueprint = f"Electricity ({ElectricitySpotPriceAverage})"
+                valueprint = f"Electricity (Gas) ({ElectricitySpotPriceAverage,fixedGasPrice})"
                 mixedUsagePriceDay.append(ElectricitySpotPriceAverage)
             else:
-                valueprint = f"Gas ({fixedGasPrice})"
+                valueprint = f"Gas (EL) ({fixedGasPrice,ElectricitySpotPriceAverage})"
                 mixedUsagePriceDay.append(fixedGasPrice)
             matrix_row.append(valueprint)        
-
-
     mixedUsagePriceDayAvg = round(sum(mixedUsagePriceDay) / len(mixedUsagePriceDay), 4)
     matrix_row.append(fixedGasPrice)
     matrix_row.append(mixedUsagePriceDayAvg)
     matrix_row.append(round(((fixedGasPrice - mixedUsagePriceDayAvg)/fixedGasPrice)*100,2))
-        
-        
     matrix.append(matrix_row)
 
-existing_header = ['00:00-02:00', '02:00-04:00', '04:00-06:00', '06:00-08:00', '08:00-10:00', '10:00-12:00', '12:00-14:00', '14:00-16:00', '16:00-18:00', '18:00-20:00', '20:00-22:00', '22:00-23:59', 'Only on Gas', 'Mixed usage', 'procentage saved']
-#print("header_row: " + str(header_row))
+existing_header = ["Day",'00:00-02:00', '02:00-04:00', '04:00-06:00', '06:00-08:00', '08:00-10:00', '10:00-12:00', '12:00-14:00', '14:00-16:00', '16:00-18:00', '18:00-20:00', '20:00-22:00', '22:00-23:59', 'Only on Gas', 'Mixed usage', 'procentage saved']
 header_row.extend(["Only on Gas", "Mixed usage", "procentage saved"])
 matrix = [list(dict.fromkeys(existing_header))] + matrix
 
